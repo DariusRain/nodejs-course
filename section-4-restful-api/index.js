c4db1dc8ea21fa420253bd36f0c40a228b5775a0
DariusRain <dariusrain.student@careerdevs.com>
2020-01-27 18:28:29 -0500

    4-7-handling-get-requests-33-10 first final commit

--- section-4-restful-api/index.js
+++ section-4-restful-api/index.js
@@ -1,54 +1,39 @@
+//Darius Rain
+//Index (v7.0.0)
+
 // Imported the express framework and invoked the value of it to the variable 'index'
 const express = require("express"),
   index = express();
 
-// Set the routes below
+//Set an array of courses
+const courses = [
+  {id: 1, course: 'course1'},
+  {id: 2, course: 'course2'},
+  {id: 3, course: 'course3'}
+]
 
-//Root route
+
+//Routes:
 index.get("/", (req, res) => {
-  req.send("Hello World");
-});
-//So what is going on here is that the 'index' variable holding the value of the full
-/*express module (Which is a giant object of values, methods, and other objects) and I want to use a method from that giant object 
-called 'get' */
-//Analogy: if you seen the matrix neo.iWantKungFu(jujitsu, judo)
-//If you didnt like that then library.book(characters) - as if your loading characters who have unique values into your own story.
-
-//In this case I am using the get method which is a type of HTTP request out of get, post, put, delete.
-//So I interpret it as valueOfExpress.httpMethod(route, callbackFunction) - index.get('/', () => {...})
-
-//Routes for RESTful API
-index.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3, 4, 5]);
+  res.send("Hello World");
 });
 
-
-
-
-
-
-
-//Route Parameters:
-//'req.params' for route parameters and 'req.query' for query strings.
-
-
-//This only send a response with one route parameter.
-
+index.get("/api/courses", (req,res) => {
+  res.send(courses)
+})
+//**NEW** 
+//The find() method is used to iterate throuh an array and return the values found based off boolean logic.
+// Syntax: array.find(eachElementOfTheArray => eachElementOfTheArray.property-value === valueOfIntrest)
+// It compares each element's property value in an array to the boolean comparison you set after seting the thick arrow. 
 index.get("/api/courses/:id", (req, res) => {
-  res.send(req.params.id);
+  const course = courses.find(course => course.id === parseInt(req.params.id)) 
+
+  if(!course){
+   return res.status(404).send(`No course with the given id of ${req.params.id}.`)
+  }
+  return res.send(course)
 });
 
-// //This has multiple route parameters
-
-index.get("/api/posts/:year/:month", (req, res) => {
-    res.send(`${req.params.year} ${req.params.month}`)
-})
-
-// //This one uses query-string paramaters
-
-index.get("/api/posts/q-string-params/:year/:month", (req, res) => {
-    res.send(req.query)
-})
 
 
 
@@ -58,11 +43,7 @@ index.get("/api/posts/q-string-params/:year/:month", (req, res) => {
 
 //Listen to a port with the .listen() method from express
 const port = process.env.PORT || 3000;
-//'process' is an object that holds another object 'env' which is the enviroment object. Then any variable name you set as the enviroment vairable can
-//be accessed. In this case 'PORT'
 
-//The purpose of this is so that your server is watching that port for incoming requests and it also uses it to make responses back as well.
-//It is sort of like how you need a phone in order for you to contact others and others to contact you otherwise you will be knocking on people's door.
 index.listen(port, () => {
   console.log(`Listening on PORT: ${port}...`);
 });
