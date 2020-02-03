e66c696785ec18c3808dd1f5500635adaea6a953
DariusRain <dariusrain.student@careerdevs.com>
2020-02-03 18:47:14 -0500

    Completed branch 6-3

--- section-6-asynchronous-javascript/async-demo/index.js
+++ section-6-asynchronous-javascript/async-demo/index.js
@@ -1,22 +1,34 @@
 //Darius Rain
-//File: index.js (More Real Example)
+//File: index.js (Callbacks)
 
 //In this example it shows why callbeacks, promises or async and await is used
 //in asynchronous operations.
 
-
 //Asynchronous:
 console.log('Before')
-console.log('Fetching github user') 
-const user = getUserId(1);
-console.log(user)
-console.log('After') 
-console.log('User is undefined becuase ther was no code set to \n handle the asynchronous event \n "setTimeOut()" just executes code in a given set time.')
-function getUserId(id) {
-    setTimeout(() =>
-    { 
-        //console.log('Fetching github user') // This line gets executed after 2 seconds (2000 ms)
-        return {userId: id, gitHubUserName: 'DariusRain'}
-    }, 2000)
+//Now that there is a callback function added to log the user
+//- once the asynchronous operation is finished.
+getUserId(1, (user) => {
+    console.log(`Found user:`, user)
+});
 
+getRepositories(1, (repos) => {
+    console.log('Repositories', repos)
+})
+console.log('After') 
+function getUserId(id, callback) {
+    //The 'callback' parameter is expecting a predefined or anonyomous function as its value.
+    console.log('Fetching github user...') 
+    setTimeout(() =>
+    {   
+        //See the anonymous function above to see how this function works.
+        callback({userId: id, gitHubUserName: 'DariusRain'})
+    }, 2000)
+}
+
+function getRepositories(id, callback){
+    console.log('Fetching repositories...')
+    setTimeout(() => {
+        callback({userId: id, repos: [1, 4, 2, 6]})
+    }, 2000)
 }
